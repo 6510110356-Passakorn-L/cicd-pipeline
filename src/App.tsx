2792db45fb1f975acbacdc9c88d22d9889952aa1
Passakorn Limseng <6510110356@email.psu.ac.th>
2023-12-17 21:44:05 +0700

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>6510110356 Passakorn Vite + React's first project.</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
